bf5a01e24c277d6f34a81cfdf0a3925cb81bcf31
Brian <brian2najera@gmail.com>
2021-06-08 17:03:07 -0500

    Added login features, cleaned startup class

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,14 +3,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+
+    //when we inherit from another c# class, we get all of its attributes, methods, and properties available in the derived class
+    public class UsersController : BaseApiController
     {   
         //Using dependency injection to inject our data context into usercontroller
         private readonly DataContext _context;
@@ -19,8 +20,10 @@ namespace API.Controllers
             _context = context;
         }
 
-        [HttpGet]
+        
         //We could of used a List<>, but it offers too many features that is not needed. We just need a simple list to return to the user. So IEnumerable fits that need. 
+        [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
@@ -28,7 +31,8 @@ namespace API.Controllers
 
 
         // api/users/3
-         [HttpGet("{id}")]
+        [Authorize]
+        [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
             return await _context.Users.FindAsync(id);
